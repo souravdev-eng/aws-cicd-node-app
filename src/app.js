3933f6e3244268b1b1c074639de3a75bef2371e0
saurav <souravmajumdar.developer@gmail.com>
2024-03-10 10:45:31 +0530

    update logs

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ app.get('/api/health', (req, res) => {
   res.status(200).send({ status: 'OK', message: 'Healthy' });
 });
 
-app.listen('4000', () => {
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
   console.log('App is running on port 4000...');
 });

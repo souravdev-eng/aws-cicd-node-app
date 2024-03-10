5b2af9ddec04c66d5c9c61b026481a98ab915972
saurav <souravmajumdar.developer@gmail.com>
2024-03-10 09:51:27 +0530

    Add health route

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/api/health', (req, res) => {
+  res.status(200).send({ status: 'OK', message: 'Healthy' });
+});
+
+app.listen('4000', () => {
+  console.log('App is running on port 4000');
+});
